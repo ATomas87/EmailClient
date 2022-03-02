8ed6ceede31be2f9f94a87cfef52a6b80b9ae5d0
ATomas87 <kombayn_nikoladze@msn.com>
2022-03-02 11:48:56 +0100

    Basic app finished

--- src/com/antonio/Launcher.java
+++ src/com/antonio/Launcher.java
@@ -1,27 +1,48 @@
 package com.antonio;
 
+import com.antonio.controller.persistence.PersistenceAccess;
+import com.antonio.controller.persistence.ValidAccount;
+import com.antonio.controller.services.LoginService;
+import com.antonio.model.EmailAccount;
+import com.antonio.model.EmailMessage;
 import com.antonio.view.ViewFactory;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Launcher extends Application {
 
     public static void main(String[] args) {
         launch(args);
     }
+    private PersistenceAccess persistenceAccess = new PersistenceAccess();
+    private EmailManager emailManager = new EmailManager();
 
     @Override
     public void start(Stage stage) throws Exception {
+        ViewFactory viewFactory = new ViewFactory(emailManager);
+        List<ValidAccount> validAccountList = persistenceAccess.loadFromPersistence();
+        if (validAccountList.size() > 0){
+            viewFactory.showMainWindow();
+            for (ValidAccount validAccount : validAccountList){
+                EmailAccount emailAccount = new EmailAccount(validAccount.getAddress(), validAccount.getPassword());
+                LoginService loginService = new LoginService(emailAccount, emailManager);
+                loginService.start();
+            }
+        } else{
+            viewFactory.showLoginWindow();
+        }
+    }
 
-        ViewFactory viewFactory = new ViewFactory(new EmailManager());
-        viewFactory.showLoginWindow();
-        viewFactory.updateStyles();
+    @Override
+    public void stop() throws Exception {
+        List<ValidAccount> validAccountList = new ArrayList<ValidAccount>();
+        for (EmailAccount emailAccount : emailManager.getEmailAccounts()){
+            validAccountList.add(new ValidAccount(emailAccount.getAddress(), emailAccount.getPassword()));
+        }
+        persistenceAccess.saveToPersistence(validAccountList);
     }
 }
